1511997c5bf6c2f45b9fe2c4f7a53167ee57c248
Jared Allen <jarmallen@gmail.com>
2016-05-05 10:39:41 -0700

    Commit again

--- Back to Beethoven/ViewController.swift
+++ Back to Beethoven/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer = AVAudioPlayer()
+    
+    @IBOutlet weak var playbackSlider: UISlider!
+    @IBOutlet weak var volumeSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        _ = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(ViewController.updatePlaybackSlider), userInfo: nil, repeats: true)
+        
+        let audioFilePath = NSBundle.mainBundle().pathForResource("beethoven-67-1-pfaul", ofType: "mp3")!
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioFilePath))
+            
+            playbackSlider.maximumValue = Float(player.duration)
+            
+        } catch {
+            
+            // Process error here
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +42,53 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func play(sender: AnyObject) {
+        
+         player.play()
+        
+    }
+   
+    @IBAction func pause(sender: AnyObject) {
+        
+        player.pause()
+        
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+        
+        player.pause()
+        
+        let audioFilePath = NSBundle.mainBundle().pathForResource("beethoven-67-1-pfaul", ofType: "mp3")!
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioFilePath))
+            
+        } catch {
+            
+            // Process error here
+            
+        }
+        
+    }
+    
+    @IBAction func adjustPlayback(sender: AnyObject) {
+        
+        player.currentTime = NSTimeInterval(playbackSlider.value)
+        
+        
+    }
+    
+    @IBAction func adjustVolume(sender: AnyObject) {
+    
+        player.volume = volumeSlider.value
+        
+    }
+    
+    func updatePlaybackSlider() {
+        
+        playbackSlider.value = Float(player.currentTime)
+        
+    }
 }
 
